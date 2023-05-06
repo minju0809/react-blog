49b688d2a4fc9790e69f059599d892446808bbd1
minju0809 <loverminju88@naver.com>
2023-05-07 00:53:35 +0900

    esm 문법 변경 중 mongoose 문제

--- blog-backend/src/api/posts/posts.ctrl.js
+++ blog-backend/src/api/posts/posts.ctrl.js
@@ -12,7 +12,7 @@ const posts = [
 /* 포스트 작성
 POST /api/posts
 { title, body } */
-exports.write = (ctx) => {
+export const write = (ctx) => {
   //REST API의 Request Body는 ctx.request.body에서 조회할 수 있음
   const { title, body } = ctx.request.body;
   postId += 1; // 기존 postId 값에 1을 더함
@@ -23,13 +23,13 @@ exports.write = (ctx) => {
 
 /* 포스트 목록 조회
 GET /api/posts */
-exports.list = ctx => {
+export const list = ctx => {
   ctx.body = posts;
 };
 
 /* 특정 포스트 조회
 GET /api/posts/:id */
-exports.read = ctx => {
+export const read = ctx => {
   const {id} = ctx.params;
   // 주어진 id 값으로 포스트를 찾음
   // 파라미터로 받아 온 값은 문자열 형식이므로 파라미터를 숫자로 변환하거나
@@ -48,7 +48,7 @@ exports.read = ctx => {
 
 /* 특정 포스트 제거
 DELETE /api/posts/:id */
-exports.remove = ctx => {
+export const remove = ctx => {
   const {id} = ctx.params;
   // 해당 id를 가진 post가 몇 번째인지 확인
   const index = posts.findIndex(p => p.id.toString() === id);
@@ -68,7 +68,7 @@ exports.remove = ctx => {
 /* 포스트 수정(교체)
 PUT /api/posts/:id
 {title, body} */
-exports.relpace = ctx => {
+export const relpace = ctx => {
   // PUT 메서드는 전체 포스트 정보를 입력하여 데이터를 통째로 교체할 때 사용
   const {id} = ctx.params;
   // 해당 id를 가진 post가 몇 번째인지 확인
@@ -93,7 +93,7 @@ exports.relpace = ctx => {
 /* 포스트 수정(특정 필드 변경)
 PATCH /api/posts/:id
 {title, body} */
-exports.update = ctx => {
+export const update = ctx => {
   // PATCH 메서드는 주어진 필드만 교체함
   const {id} = ctx.params;
   // 해당 id를 가진 post가 몇 번째인지 확인
