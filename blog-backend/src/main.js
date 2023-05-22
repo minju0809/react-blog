60a5b8809641f7c75c8c5d50977a759daf3af9df
minju0809 <loverminju88@naver.com>
2023-05-22 16:07:32 +0900

    리액트를다루는기술 종료

--- blog-backend/src/main.js
+++ blog-backend/src/main.js
@@ -1,11 +1,11 @@
 require('dotenv').config();
-
 import Koa from 'koa';
 import Router from 'koa-router';
 import bodyParser from 'koa-bodyparser';
 import mongoose from 'mongoose';
 
 import api from './api';
+import createFakeData from './createFakeData';
 
 // 비구조화 할당을 통해 process.env 내부 값에 대한 레퍼런스 만들기
 const { PORT, MONGO_URI } = process.env;
@@ -14,6 +14,7 @@ mongoose
   .connect(MONGO_URI)
   .then(() => {
     console.log('Connected to MongoDB');
+    // createFakeData();
   })
   .catch((e) => {
     console.error(e);
